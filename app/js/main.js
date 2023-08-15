2334844518622c85a11dcf4b8bdc36f541025a83
aboba88 <ilyapanama88@gmail.com>
2023-08-15 05:26:41 +0300

    add_other_sections

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,22 @@ const menuOpen = document.querySelector('.nav__burger');
 menuBtn.addEventListener('click' , () => {
     menuOpen.classList.toggle('burger--open');
 });
+
+
+
+const tabButtons = document.querySelectorAll(".tab-button");
+const tabContents = document.querySelectorAll(".tab-content");
+
+tabButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const tabId = button.getAttribute("data-tab");
+        tabContents.forEach(content => {
+            content.classList.remove("active");
+        });
+        document.getElementById(tabId).classList.add("active");
+    });
+});
+
+
+
+
